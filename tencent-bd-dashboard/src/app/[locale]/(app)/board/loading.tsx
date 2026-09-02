241970016d92398c91627e271786497e94cadc52
dywongcloud <dywong@global.tencent.com>
2026-09-02 15:47:30 -0700

    Delete tencent-bd-dashboard directory

--- tencent-bd-dashboard/src/app/[locale]/(app)/board/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { SkeletonBoard, SkeletonPage } from '@/components/Skeleton';
-
-export default function BoardLoading() {
-  return (
-    <SkeletonPage>
-      <SkeletonBoard />
-    </SkeletonPage>
-  );
-}
--- tencent-bd-dashboard/src/app/[locale]/(app)/weekly/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { SkeletonCards, SkeletonPage } from '@/components/Skeleton';
-
-export default function WeeklyLoading() {
-  return (
-    <SkeletonPage>
-      <SkeletonCards count={9} />
-    </SkeletonPage>
-  );
-}
